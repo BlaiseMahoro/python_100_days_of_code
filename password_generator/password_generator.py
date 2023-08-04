d15d79de91d166ca74e17460f71e1a17988c523d
blaisemahoro <bmahoro123@gmail.com>
2023-08-04 00:35:56 -0500

    init commit for password generator

---
+++ password_generator/password_generator.py
@@ -0,0 +1,48 @@
+
+import random
+
+upper_case_letters = [chr(i) for i in range(65, 91)]
+lower_case_letters = [chr(i) for i in range(97, 123)]
+
+letters = upper_case_letters + lower_case_letters
+numbers = [chr(i) for i in range(48, 58)]
+special_symbols = [
+    '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', 
+    '_', '-', '+', '=', '{', '[', '}', ']', '|', ':', 
+    ';', '"', '\'', '<', ',' ,'>', '.', '?', '/'
+    ]
+numbers = [chr(i) for i in range(48, 58)]
+chars = [letters, numbers, special_symbols]
+
+
+'''
+This function uses pseudo-random number generator which is not good for security because one can predict it.
+To make this password generator, one can make stronger password use a physical activity be its seed for random number generation.
+'''
+def generate_password():
+    letters_num = int(input('How many letters would you like in your password?\n'))
+    digits_num = int(input('How many digits would you like in your password?\n'))
+    special_num = int(input('How many special symbols would you like in your password?\n'))
+
+    char_nums = [letters_num, digits_num, special_num]
+
+    gen_password = []
+
+    while any(char_nums):
+        i = random.randint(0, len(chars) - 1)
+        j = random.randint(0, len(chars[i]) - 1)
+        if not char_nums[i]:
+            continue
+
+        gen_password.append(chars[i][j])
+        char_nums[i] -= 1
+
+    return gen_password
+        
+
+def main():
+    print('*****************Welcome to the PyPassword Generator!*****************')
+    new_pass = generate_password()
+    print(new_pass)
+
+main()
