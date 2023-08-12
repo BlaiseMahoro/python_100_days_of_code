32448e8f90de4081c1a4ac72cb4ea3fc42c3e241
blaisemahoro <bmahoro123@gmail.com>
2023-08-11 23:16:20 -0500

    added hanger

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -3,6 +3,73 @@ from collections import defaultdict
 from word_generator import WordGenerator
 import random
 
+HANGING_LEVELS = [
+    '''''',
+    '''
+    +-------+
+    |       |
+    |       O
+    |
+    |
+    |
+    |
+    ''',
+    '''
+    +-------+
+    |       |
+    |       O
+    |       |
+    |
+    |
+    |
+    ''',
+    '''
+    +-------+
+    |       |
+    |       O
+    |       |/
+    |
+    |
+    |
+    ''',
+    '''
+    +-------+
+    |       |
+    |       O
+    |      \|/
+    |
+    |
+    |
+    ''',
+       '''
+    +-------+
+    |       |
+    |       O
+    |      \|/
+    |       |
+    |
+    |
+    ''',
+    '''
+    +-------+
+    |       |
+    |       O
+    |      \|/
+    |       |
+    |      /
+    |
+    ''',
+      '''
+    +-------+
+    |       |
+    |       O
+    |      \|/
+    |       |
+    |      / \\
+    |
+    ''']
+
+
 def game():
     
     word_generator = WordGenerator()
@@ -14,7 +81,7 @@ def game():
     
     puzzle_word = ''.join(random.choice(words))
     
-    print('puzzle Word: ', puzzle_word) # comment out this to challenge yourself
+    # print('puzzle Word: ', puzzle_word) # uncomment this to test correctness
     word_indexes_map = defaultdict(list)
     
     for i, l in enumerate(puzzle_word):
@@ -25,28 +92,28 @@ def game():
     guessed = ['_'] * word_len
     
     wrong_guesses = 0
+    seen = set()
     
     while word_len:
+        print('****************************************************')
+        print(HANGING_LEVELS[wrong_guesses], end='\t\t\t\t')
         print(' '.join(guessed))
         guess_letter = input('Guess a letter: ').lower()
-        if guess_letter not in word_indexes_map:
+        if guess_letter not in word_indexes_map or guess_letter in seen:
             wrong_guesses += 1 
-            if wrong_guesses == 5:
-                print('Failed to solve the puzzle. Try again')
+            if wrong_guesses == len(HANGING_LEVELS):
+                print('Failed to solve the puzzle. Try again. \nThe word was ' + puzzle_word)
                 return
             continue
         for idx in word_indexes_map[guess_letter]:
             guessed[idx] = guess_letter
             word_len -= 1
+        seen.add(guess_letter)
             
-    print('Good job! You guessed the word.')     
-    print(''.join(guessed))
-            
+    print('Good job! You guessed the word.', end='\t\t')     
+    print(''.join(guessed))           
 
         
-
-
-
 def main():
     game()
 
